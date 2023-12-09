33854680b31f67285c7126051f7bd7c79379ddcb
Jhonatan Acuña <jacuna61@ucatolica.edu.co>
2023-12-09 15:31:33 -0500

    Added readme

---
+++ CreateDatabase.sql
@@ -0,0 +1,2 @@
+drop database if exists prueba;
+create database prueba;
--- src/main/java/com/prueba/rickandmorty/repository/CharacterRepository.java
+++ src/main/java/com/prueba/rickandmorty/repository/CharacterRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CharacterRepository extends JpaRepository<CharacterDB, Long> {
-
     @Query(value = "SELECT EXISTS(SELECT c.name FROM character c WHERE c.name = ?1) ", nativeQuery = true)
     Boolean findByName(String name);
 }
